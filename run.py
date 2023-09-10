49221fe9430d20a635c0aa82660e60f3ed3b1e9d
Alexff <matveevalexander79@gmail.com>
2023-09-10 18:36:13 +0300

    add create func

--- run.py
+++ run.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', the_title='Главная')
 
+def create():
+    return
+
 if __name__ == '__main__':
     app.run(debug=True, port=5667)
